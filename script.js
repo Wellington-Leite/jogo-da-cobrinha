4c7ae0c2092f34b3efc89e650bc0ffa9ba66ef1d
Wellington Leite <wellingtonleite15@gmail.com>
2020-12-16 22:46:54 -0300

    Movimentos cobrinha

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ snake[0] = {
   y: 8 * box,
 };
 
+let direction = "right";
+
 function criarBG() {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
@@ -19,5 +21,45 @@ function criarCobrinha() {
   }
 }
 
-criarBG();
-criarCobrinha();
+document.addEventListener("keydown", update);
+
+function update(event) {
+  // 37 para a direita, 38 para baixo, 39 para esquerda, 40 para cima
+  if (event.keyCode == 37 && direction != "right") direction = "left";
+  if (event.keyCode == 38 && direction != "down") direction = "up";
+  if (event.keyCode == 39 && direction != "left") direction = "right";
+  if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function inicarJogo() {
+  // Mantendo a cobrinha dentro do canvas
+  if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+  criarBG();
+  criarCobrinha();
+
+  //Setando a posicão da cobrinha
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  //Criando as coordenadas
+  if (direction == "right") snakeX += box;
+  if (direction == "left") snakeX -= box;
+  if (direction == "up") snakeY -= box;
+  if (direction == "down") snakeY += box;
+
+  snake.pop(); //Retirando o Ultimo elemento
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+
+  //Acrescentando um elemento a frente
+  snake.unshift(newHead);
+}
+
+let jogo = setInterval(inicarJogo, 500);
