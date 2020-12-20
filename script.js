86c9ef4e6ab4a1ce82d733c453a5e2a0dd3dfd7d
Wellington Leite <wellingtonleite15@gmail.com>
2020-12-20 14:25:17 -0300

    Fix

--- script.js
+++ script.js
@@ -18,13 +18,13 @@ let food = {
 };
 
 function criarBG() {
-  context.fillStyle = "lightgreen";
+  context.fillStyle = "white";
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
   for (let i = 0; i < snake.length; i++) {
-    context.fillStyle = "black";
+    context.fillStyle = "lime";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
 }
